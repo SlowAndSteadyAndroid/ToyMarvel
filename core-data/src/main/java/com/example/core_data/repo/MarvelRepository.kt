0c0c4fe5dfda49e7197d3f390cc6e76cfce216b8
Parkduksung <duksung1234@naver.com>
2023-01-03 04:15:54 +0900

    [feature] data 작업 추가.

--- core-data/src/main/java/com/example/core_data/repo/MarvelRepository.kt
+++ core-data/src/main/java/com/example/core_data/repo/MarvelRepository.kt
@@ -1,4 +1,15 @@
 package com.example.core_data.repo
 
+import com.example.core_database.room.entity.MarvelEntity
+import com.example.core_model.marvel.response.CharacterResponse
+import kotlinx.coroutines.flow.Flow
+
 interface MarvelRepository {
+
+    val bookmarkList: Flow<List<MarvelEntity>>
+
+    fun getAllCharacters(
+        offset: Int
+    ): Flow<CharacterResponse>
+
 }
--- core-data/src/main/java/com/example/core_data/repo/MarvelRepositoryImpl.kt
+++ core-data/src/main/java/com/example/core_data/repo/MarvelRepositoryImpl.kt
@@ -1,4 +1,20 @@
 package com.example.core_data.repo
 
-class MarvelRepositoryImpl : MarvelRepository {
+import com.example.core_database.room.dao.MarvelDao
+import com.example.core_database.room.entity.MarvelEntity
+import com.example.core_model.marvel.response.CharacterResponse
+import com.example.core_network.api.MarvelApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class MarvelRepositoryImpl @Inject constructor(private val marvelApi: MarvelApi,private val marvelDao: MarvelDao) :
+    MarvelRepository {
+
+    override fun getAllCharacters(offset: Int): Flow<CharacterResponse> = flow {
+        emit(marvelApi.getAllCharacters(offset))
+    }
+
+    override val bookmarkList: Flow<List<MarvelEntity>>
+        get() = marvelDao.getAllBookmark()
 }
