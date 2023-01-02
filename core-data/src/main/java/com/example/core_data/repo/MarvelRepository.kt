6581d738230b93dbeccd1ad3c89bccfd61d00fa0
Parkduksung <duksung1234@naver.com>
2023-01-03 04:54:16 +0900

    [feature] data 즐겨찾기 구성 추가.

--- core-data/src/main/java/com/example/core_data/repo/MarvelRepository.kt
+++ core-data/src/main/java/com/example/core_data/repo/MarvelRepository.kt
@@ -1,6 +1,7 @@
 package com.example.core_data.repo
 
 import com.example.core_database.room.entity.MarvelEntity
+import com.example.core_model.marvel.model.CharacterItem
 import com.example.core_model.marvel.response.CharacterResponse
 import kotlinx.coroutines.flow.Flow
 
@@ -11,5 +12,9 @@ interface MarvelRepository {
     fun getAllCharacters(
         offset: Int
     ): Flow<CharacterResponse>
+
+    suspend fun insertCharacterItem(item: CharacterItem): Boolean
+
+    suspend fun deleteCharacterItem(item: CharacterItem): Boolean
 
 }
--- core-data/src/main/java/com/example/core_data/repo/MarvelRepositoryImpl.kt
+++ core-data/src/main/java/com/example/core_data/repo/MarvelRepositoryImpl.kt
@@ -2,13 +2,17 @@ package com.example.core_data.repo
 
 import com.example.core_database.room.dao.MarvelDao
 import com.example.core_database.room.entity.MarvelEntity
+import com.example.core_model.marvel.model.CharacterItem
 import com.example.core_model.marvel.response.CharacterResponse
 import com.example.core_network.api.MarvelApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class MarvelRepositoryImpl @Inject constructor(private val marvelApi: MarvelApi,private val marvelDao: MarvelDao) :
+class MarvelRepositoryImpl @Inject constructor(
+    private val marvelApi: MarvelApi,
+    private val marvelDao: MarvelDao
+) :
     MarvelRepository {
 
     override fun getAllCharacters(offset: Int): Flow<CharacterResponse> = flow {
@@ -17,4 +21,24 @@ class MarvelRepositoryImpl @Inject constructor(private val marvelApi: MarvelApi,
 
     override val bookmarkList: Flow<List<MarvelEntity>>
         get() = marvelDao.getAllBookmark()
+
+    override suspend fun insertCharacterItem(item: CharacterItem): Boolean =
+        try {
+            val result = marvelDao.insertBookmark(item.asCharacterEntity())
+            result > 0
+        } catch (e: Exception) {
+            false
+        }
+
+    override suspend fun deleteCharacterItem(item: CharacterItem): Boolean =
+        try {
+            val result = marvelDao.deleteBookmark(item.asCharacterEntity())
+            result >= 0
+        } catch (e: Exception) {
+            false
+        }
 }
+
+
+fun CharacterItem.asCharacterEntity(): MarvelEntity =
+    MarvelEntity(id, name, image, comics, series, stories, events, urls)
